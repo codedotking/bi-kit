df28a45543454766733ab13ef96fb2d68d3e5873
he.wenyao <he.wenyao@nint.com>
2024-12-13 19:31:06 +0800

    add workbench

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects() {
+        return [
+            {
+                source: '/', // 源路径
+                destination: '/workbench', // 目标路径
+                permanent: true, // 是否为永久重定向（301），默认为 false（302）
+            },
+        ]
+    }
+
+
+};
 
 export default nextConfig;
